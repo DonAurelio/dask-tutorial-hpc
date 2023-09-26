55addb6550464aaea124476a52affc050c680ed3
Aurelio Vivas <aurelio.vivas@correounivalle.edu.co>
2023-09-26 14:42:23 -0500

    Update (#2)
    * update functional programming example, reduce length of the matrices
    
    * update
    
    * update
    
    * remove cloud example
    
    * scalability finished
    
    * move old notebooks versions
    
    * satellite image processing use case
    
    * ready to go
    
    * finaly

--- source/1/0.matrix_sum_python.py
+++ source/1/0.matrix_sum_python.py
@@ -2,21 +2,17 @@
 # coding: utf-8
 
 # __1. Import required libraries, define required variables and functions__
-import numpy as np 
 import sys
 
 # Size of the array
-shape = (10000,10000)
+shape = (1000,1000)
 
 # __2. Create arrays, list of lists__
 x = [ [1 for x in range(0,shape[0])] for y in range(0,shape[1]) ]
 y = [ [1 for x in range(0,shape[0])] for y in range(0,shape[1]) ]
-z = [ [0 for x in range(0,shape[0])] for y in range(0,shape[1]) ]
 
 # __2. Write the sequential program to find the value of $z$__
-for i in range(0,shape[0]):
-    for j in range(0,shape[1]):
-        z[i][j] = x[i][j]**2 + y[i][j]**2
+z = [ [(x[i][j]**2 + y[i][j]**2) for i in range(0,shape[0])] for j in range(0,shape[1]) ]
 
 print(f"Size of X {sys.getsizeof(x) / 1000000000} GB")
 print(f"Size of Y {sys.getsizeof(y) / 1000000000} GB")
--- source/1/3.matrix_sum_dask_delayed.py
+++ source/1/3.matrix_sum_dask_delayed.py
@@ -6,31 +6,28 @@
 import numpy as np
 import dask
 
-shape = (10000,10000)
+shape = (1000,1000)
 
-def squared(a):
-    return a**2
+def create_array():
+    return [ [1 for x in range(0,shape[0])] for y in range(0,shape[1]) ]
+
+def square(a):
+    # __1. Square array (element-wise)__
+    return [ [ (a[x][y]**2) for x in range(0,shape[0])] for y in range(0,shape[1]) ]
 
 def add(a,b):
-    return a + b
+    # __1. Addition (element-wise)__
+    return [ [ (a[x][y] + b[x][y]) for x in range(0,shape[0])] for y in range(0,shape[1]) ]
 
-# __2. Create arrays__
+# __2. Parallelize the previous program__ using Dask Delayed.
 
-x = np.ones(shape=shape)
-y = np.ones(shape=shape)
-
-# __3. Write the equation in terms of functions__
-
-a = squared(x)
-b = squared(y)
-c = add(a,b)
-
-# __4. Parallelize the previous program__ using Dask Delayed.
-
-a = dask.delayed(squared)(x)
-b = dask.delayed(squared)(y)
+x = dask.delayed(create_array)()
+y = dask.delayed(create_array)()
+a = dask.delayed(square)(x)
+b = dask.delayed(square)(y)
 c = dask.delayed(add)(a,b)
 
-# __5. Execute the computation__
-
+# __3. Execute the computation__
+# c.compute(scheduler='threads')
+# c.compute(scheduler='processes')
 c.compute()
