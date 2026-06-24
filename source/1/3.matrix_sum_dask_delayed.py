3cf259ed84714c304561180e974218b14c4a1a1b
Mauricio <mdcorreah@eafit.edu.co>
2026-06-23 23:26:08 -0500

    Fix row and column indexing in matrix examples

--- source/1/3.matrix_sum_dask_delayed.py
+++ source/1/3.matrix_sum_dask_delayed.py
@@ -3,21 +3,20 @@
 
 # __1. Import required libraries, define required variables and functions__
 
-import numpy as np
 import dask
 
 shape = (1000,1000)
 
 def create_array():
-    return [ [1 for x in range(0,shape[0])] for y in range(0,shape[1]) ]
+    return [[1 for column in range(0, shape[1])] for row in range(0, shape[0])]
 
 def square(a):
     # __1. Square array (element-wise)__
-    return [ [ (a[x][y]**2) for x in range(0,shape[0])] for y in range(0,shape[1]) ]
+    return [[ (a[row][column]**2) for column in range(0, shape[1])] for row in range(0, shape[0]) ]
 
 def add(a,b):
     # __1. Addition (element-wise)__
-    return [ [ (a[x][y] + b[x][y]) for x in range(0,shape[0])] for y in range(0,shape[1]) ]
+    return [[ (a[row][column] + b[row][column]) for column in range(0, shape[1])] for row in range(0, shape[0]) ]
 
 # __2. Parallelize the previous program__ using Dask Delayed.
 
